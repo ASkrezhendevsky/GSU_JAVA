d8db9c5ad193bbe80c05ee7957a7ebd86147f8a6
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-04-21 12:57:31 +0300

    Word class added

---
+++ idz_4/src/by/gsu/pms/Word.java
@@ -0,0 +1,22 @@
+package by.gsu.pms;
+
+public class Word {
+    private String text;
+
+    public Word(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
