887acda42a216b277dd6bd612414accceb1cb39e
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-04-26 09:22:40 +0300

    Char deletion added

--- idz_4/src/by/gsu/pms/Main.java
+++ idz_4/src/by/gsu/pms/Main.java
@@ -2,6 +2,8 @@ package by.gsu.pms;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +12,15 @@ public class Main {
         try (Scanner scanner = new Scanner(new FileReader(inputFile))){
             Paragraph paragraph = new Paragraph(scanner);
             System.out.println(paragraph.toString());
+
+            for (Sentence sentence: paragraph.sentences) {
+                for (Word word: sentence.words) {
+                    WordCharDeleter.deleteFirstChar(word);
+                }
+            }
+
+            System.out.println(paragraph.toString());
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
